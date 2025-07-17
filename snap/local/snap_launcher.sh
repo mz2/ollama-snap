aa65e4c846de379cb07ad86f2528a694f4d6ca1c
Matias Piipari <matias.piipari@gmail.com>
2025-07-17 11:29:33 +0300

    Adds snap config option for setting OLLAMA_CONTEXT_LENGTH

--- snap/local/snap_launcher.sh
+++ snap/local/snap_launcher.sh
@@ -6,9 +6,14 @@ OLLAMA_MODELS=$(snapctl get models)
 OLLAMA_ORIGINS=$(snapctl get origins)
 CUDA_VISIBLE_DEVICES_VALUE=$(snapctl get cuda-visible-devices)
 OLLAMA_DEBUG=$(snapctl get debug)
+OPTIONAL_CONTEXT_LENGTH=$(snapctl get context-length)
 
 if [ -n "$CUDA_VISIBLE_DEVICES_VALUE" ]; then
     export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES_VALUE
+fi
+
+if [ -n "$OPTIONAL_CONTEXT_LENGTH" ]; then
+    export OLLAMA_CONTEXT_LENGTH=$OPTIONAL_CONTEXT_LENGTH
 fi
 
 export OLLAMA_HOST OLLAMA_MODELS OLLAMA_ORIGINS OLLAMA_DEBUG
